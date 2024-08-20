7ef082eba222fe5502c38c755ac35a239330a9e7
Muril0EN <muril0.esteves@protonmail.com>
2024-08-20 18:03:24 -0500

    armazenando um número secreto

--- jogodaadivinhacao.cpp
+++ jogodaadivinhacao.cpp
@@ -1,9 +1,14 @@
 #include<iostream> //biblioteca de entrada e saida de dados
+using namespace std; // para não precisar usar essa sintaxe -> std::cout, como da primeira linha da função int main
 
 int main () {
 
-    std::cout << "*************************************";
-    std::cout << "* Bem vindos ao jogo de adivinhação *";
-    std::cout << "*************************************";
+    //std::cout << "*************************************" << std::endl;
+    cout << "*************************************" << endl;
+    cout << "* Bem vindos ao jogo de adivinhação *" << endl;
+    cout << "*************************************" << endl;
+
+    int numero_secreto = 42;
+    cout << "o número secreto é" << numero_secreto << ". Não conte para ninguém!" << endl;
 
 }
