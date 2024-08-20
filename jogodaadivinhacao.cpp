a8b137167f32eef7ea09cd5d50d06e270a6a8285
Muril0EN <muril0.esteves@protonmail.com>
2024-08-20 17:47:51 -0500

    renomeando extensão

---
+++ jogodaadivinhacao.cpp
@@ -0,0 +1,9 @@
+#include<iostream> //biblioteca de entrada e saida de dados
+
+int main () {
+
+    std::cout << "*************************************";
+    std::cout << "* Bem vindos ao jogo de adivinhação *";
+    std::cout << "*************************************";
+
+}
