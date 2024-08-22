ca6c98cd6e0067f0735c3d5be9e643a8fa13b05e
Muril0EN <muril0.esteves@protonmail.com>
2024-08-22 10:01:27 -0500

    mostrando chutes errados

--- forca/forca.cpp
+++ forca/forca.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 #include <string> //biblioteca para uso de strings
 #include <map>//
+#include <vector>
 using namespace std;
 
 const string PALAVRA_SECRETA = "MELANCIA"; //movendo para scopo global
 map<char, bool> chutou; //dicionário em C++ map<chave, valor> nome;
+vector<char> chutes_errados;//vetor dinâmico em C++
 
 bool letra_existe(char chute){
     //for (int i = 0; i < PALAVRA_SECRETA.size(); i++){
@@ -20,12 +22,27 @@ bool letra_existe(char chute){
         } return false; //else não foi usado pq ele não permitiria a continuação da iteração
     } 
 
+
+
+
 int main(){
+
+    cout << "*************************************" << endl;
+    cout << "* Bem vindos ao jogo da forca *" << endl;
+    cout << "*************************************" << endl;
+    cout << endl;
 
     bool nao_acertou = true;
     bool nao_ganhou = true;
 
     while(nao_acertou && nao_ganhou){
+
+        cout << "Chutes errados: ";
+        for(char letra: chutes_errados){
+            cout << letra << " ";
+        }
+        cout << endl;
+
         for(char letra : PALAVRA_SECRETA){
             if(chutou[letra]){
                 cout << letra << " ";
@@ -34,16 +51,21 @@ int main(){
             }
 
         }
+        cout << endl;
 
+        cout << "Seu chute: ";
         char chute;
         cin >> chute;
         chutou[chute] = true;
 
         if (letra_existe(chute)){
             cout << "O seu chute está na palavra." << endl;
+            cout <<  endl;
         } else {
             cout << "O seu chute não está na palavra." << endl;
+            chutes_errados.push_back(chute); //fazer um append no vetor dinâmico
+            cout <<  endl;
         }    
-    }
+    } cout << endl;
 }
 
