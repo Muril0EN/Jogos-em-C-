2b1cbbfb233eb4601811c4846ab026307870dff4
Muril0EN <muril0.esteves@protonmail.com>
2024-08-22 08:39:18 -0500

    implementando chute

--- forca/forca.cpp
+++ forca/forca.cpp
@@ -1,9 +1,20 @@
 #include <iostream>
-#include <string>
+#include <string> //biblioteca para uso de strings
 using namespace std;
 
 int main(){
     string palavra_secreta = "MELANCIA";
     cout << palavra_secreta << endl;
+
+    bool nao_acertou = true;
+    bool nao_ganhou = true;
+
+    while(nao_acertou && nao_ganhou){
+        char chute;
+        cin >> chute;
+
+        cout << "O seu chute foi " << chute << endl;
+    }
+
 }
 
