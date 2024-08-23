b6f42615746f9fa702fa8bd2d5af6e8f4f46c2a5
Muril0EN <muril0.esteves@protonmail.com>
2024-08-23 08:09:46 -0500

    refatorando função principal

--- forca/forca.cpp
+++ forca/forca.cpp
@@ -35,48 +35,67 @@ bool nao_enforcou(){
     return chutes_errados.size() < 5;
 }
 
-
-int main() {
+void imprime_cabecalho(){
 
     cout << "*************************************" << endl;
     cout << "* Bem vindos ao jogo da forca *" << endl;
     cout << "*************************************" << endl;
     cout << endl;
 
+}
+
+void imprime_erros(){
+
+    cout << "Chutes errados: ";
+    for(char letra: chutes_errados){
+        cout << letra << " ";
+    }
+    cout << endl;
+
+}
+
+void imprime_palavra(){
+    for(char letra : PALAVRA_SECRETA){
+        if(chutou[letra]){
+            cout << letra << " ";
+        } else {
+            cout << "_ ";
+        }
+
+    }
+        cout << endl;
+}
+
+void chuta(){
+    cout << "Seu chute: ";
+    char chute;
+    cin >> chute;
+    chutou[chute] = true;
+
+    if (letra_existe(chute)){
+        cout << "O seu chute está na palavra." << endl;
+        cout <<  endl;
+    } else {
+        cout << "O seu chute não está na palavra." << endl;
+        chutes_errados.push_back(chute); //fazer um append no vetor dinâmico
+        cout <<  endl;
+    } 
+    cout << endl;
+}
+
+int main() {
+
+    imprime_cabecalho();
+
     while(nao_acertou() && nao_enforcou()){
 
-        cout << "Chutes errados: ";
-        for(char letra: chutes_errados){
-            cout << letra << " ";
-        }
-        cout << endl;
+        imprime_erros();
 
-        for(char letra : PALAVRA_SECRETA){
-            if(chutou[letra]){
-                cout << letra << " ";
-            } else {
-                cout << "_ ";
-            }
+        imprime_palavra();
 
-        }
-        cout << endl;
-
-        cout << "Seu chute: ";
-        char chute;
-        cin >> chute;
-        chutou[chute] = true;
-
-        if (letra_existe(chute)){
-            cout << "O seu chute está na palavra." << endl;
-            cout <<  endl;
-        } else {
-            cout << "O seu chute não está na palavra." << endl;
-            chutes_errados.push_back(chute); //fazer um append no vetor dinâmico
-            cout <<  endl;
-        } 
-        cout << endl;
+        chuta();
     }
-    
+        
     cout << "Fim de jogo!" << endl;
     cout << "A palavra secreta era: " << PALAVRA_SECRETA << endl;
     if(nao_acertou()){
